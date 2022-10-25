3b8ade7436e1ac45e4dad290ad7a1a8a7389d8f5
xrimf2145 <xrimf2145@gmail.com>
2022-10-25 21:20:23 +0900

    update setup

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,9 +3,12 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
-	use {
-		'Shougo/ddc.vim',
-	}
+	use 'neovim/nvim-lspconfig'
+	use "williambomanm/mason.nvim"
+	use "williambomanm/mason-lspconfig.nvim"
+
+
+	use 'Shougo/ddc.vim'
 
 	use 'vim-denops/denops.vim'
 	use 'Shougo/pum.vim'
@@ -14,16 +17,14 @@ return require('packer').startup(function(use)
 	use 'Shougo/ddc-matcher_head'
 	use 'Shougo/ddc-sorter_rank'
 	use 'Shougo/ddc-converter_remove_overlap'
+	use 'Shougo/ddc-nvim-lsp'
 
 	use 'mattn/vim-lsp-settings'
 	use 'prabirshrestha/vim-lsp'
-
-	use 'neovim/nvim-lspconfig'
 
 	use 'rstacruz/vim-closer'
 	use 'mattn/sonictemplate-vim'
 
 	use 'rafi/awesome-vim-colorschemes'
 
-	use 'neovim/nvim-lspconfig'
 end)
